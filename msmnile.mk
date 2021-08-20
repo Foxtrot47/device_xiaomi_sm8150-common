c77cc720b5c1cdbf124206a712fc591483d937a8
Foxtrot47 <jjneutron@outlook.com>
2021-08-20 20:37:15 +0530

    sm8150-common: Use common QTI Telephony component

--- msmnile.mk
+++ msmnile.mk
@@ -203,8 +203,6 @@ PRODUCT_COPY_FILES += \
 
 # IPACM
 PRODUCT_PACKAGES += \
-    ipacm \
-    IPACM_cfg.xml \
     libipanat \
     liboffloadhal
 
@@ -280,7 +278,6 @@ PRODUCT_COPY_FILES += \
 
 # QMI
 PRODUCT_PACKAGES += \
-    libjson \
     libqti_vndfwk_detect \
     libqti_vndfwk_detect.vendor \
     libvndfwk_detect_jni.qti \
@@ -292,12 +289,12 @@ TARGET_COMMON_QTI_COMPONENTS := \
     av \
     display \
     gps \
-    media-legacy
+    media-legacy \
+    telephony
 
 # RIL
 PRODUCT_PACKAGES += \
     libprotobuf-cpp-full \
-    librmnetctl \
     libxml2
 
 # Rootdir
@@ -335,19 +332,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
     hardware/xiaomi
-
-# Telephony
-PRODUCT_PACKAGES += \
-    ims-ext-common \
-    ims_ext_common.xml \
-    qti-telephony-hidl-wrapper \
-    qti_telephony_hidl_wrapper.xml \
-    qti-telephony-utils \
-    qti_telephony_utils.xml \
-    telephony-ext
-
-PRODUCT_BOOT_JARS += \
-    telephony-ext
 
 # USB
 PRODUCT_PACKAGES += \
